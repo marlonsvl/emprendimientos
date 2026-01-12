efaba345f49a293aa4569079e97d4d6ad2fa9659
Santigo Viñan <santigovinan@Santigos-MacBook-Pro.local>
2026-01-12 10:58:46 -0500

    reset password

--- gastronomia/email.py
+++ gastronomia/email.py
@@ -76,7 +76,7 @@ The {context['site_name']} Team
 
 
 class PasswordResetEmail(BaseEmailMessage):
-    template_name = "email/password_reset_email.html"
+    template_name = "email/password_reset_email_body.html"
     def get_context_data(self):
         context = super().get_context_data()
         user = context.get("user")
